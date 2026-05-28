06593d4df3d90e8225635698c60d79fde22b8e6d
bichphan295-svg <bichphan.295@gmail.com>
2026-05-29 05:46:39 +0700

    Add files via upload

---
+++ poems.js
@@ -0,0 +1,138 @@
+const poems = [
+
+`"Yêu là chết ở trong lòng một ít,
+Vì mấy khi yêu mà chắc được yêu?
+Cho rất nhiều, song chẳng nhận bao nhiêu;
+Người ta phụ, hoặc thờ ơ, chẳng biết."
+
+- Xuân Diệu -`,
+
+`Máu đã khô rồi thơ cũng khô
+Tình ta chết yểu tự bao giờ!
+Từ nay trong gió, - trong mây gió,
+Lời thảm thương rền khắp nẻo mơ.
+
+- Hàn Mặc Tử -`,
+
+`Tôi rót hồn tôi xuống mắt nàng.
+Hồn tôi là cả một lời van.
+Tôi van nàng đấy! Van nàng đấy!
+Ai có yêu đương chả vội vàng?
+
+Tôi rót hồn tôi xuống đã nhiều,
+Hồn tôi còn có được bao nhiêu?
+Tôi đi sợ cả lời tôi nói,
+Sợ cả gần nàng, sợ cả yêu.
+
+- Nguyễn Bính -`,
+
+`"Anh nằm ngoài sự thực
+Em ngồi trong chiêm bao
+Cách xa nhau biết mấy
+Nhớ thương quá thì sao?
+
+Anh nuốt phứt hàng chữ
+Anh cắn vỡ lời thơ
+Anh cắn, cắn cắn cắn
+Hơi thở đứt làm tư!"
+
+- Hàn Mặc Tử -`,
+
+`"Trời hỡi, bao giờ tôi chết đi?
+Bao giờ tôi hết được yêu vì,
+Bao giờ mặt nhựt tan thành máu,
+Và khối lòng tôi cứng tợ si?"
+
+- Hàn Mặc Tử -`,
+
+`"Họ đã xa rồi khôn níu lại,
+Lòng thương chưa đã, mến chưa bưa...
+Người đi, một nửa hồn tôi mất,
+Một nửa hồn tôi bỗng dại khờ."
+
+- Hàn Mặc Tử -`,
+
+`Ta trút linh hồn giữa lúc đây,
+Gió sầu vô hạn nuối trong cây...
+- Còn em sao chẳng hay gì cả?
+Xin để tang anh đến vạn ngày.
+
+- Hàn Mặc Tử -`,
+
+`Tôi trả cho tôi những ngại ngần
+Trả người - đây nhé những phân vân
+Cõi riêng lặng lẽ gài then kín
+Ngoài ấy người vui với bụi trần.
+
+- Hàn Mặc Tử -`,
+
+`Tôi khát vô cùng
+Tôi giết thời gian trong nắm tay
+Tôi vò tiếc mến như vo lụa
+Cất tiếng cười giòn xao động vùng mây
+
+- Hàn Mặc Tử -`,
+
+`Sóng bắt đầu từ gió
+Gió bắt đầu từ đâu?
+Em cũng không biết nữa
+Khi nào ta yêu nhau
+
+- Xuân Quỳnh -`,
+
+`Ở ngoài kia đại dương
+Trăm nghìn con sóng đó
+Con nào chẳng tới bờ
+Dù muôn vời cách trở
+
+- Xuân Quỳnh -`,
+
+`Nếu tất cả đường đời đều trơn láng,
+Chắc gì ta đã nhận ra ta
+
+- Lưu Quang Vũ -`,
+
+`Dù đục dù trong, con sông vẫn chảy
+Dù cao dù thấp, cây lá vẫn xanh
+Dù người phàm tục hay kẻ tu hành
+Vẫn phải sống từ những điều rất nhỏ
+
+- Lưu Quang Vũ -`,
+
+`Chỉ có thuyền mới hiểu
+Biển mênh mông nhường nào
+Chỉ có biển mới biết
+Thuyền đi đâu, về đâu
+
+Những ngày không gặp nhau
+Biển bạc đầu thương nhớ
+Những ngày không gặp nhau
+Lòng thuyền đau - rạn vỡ
+
+- Xuân Quỳnh -`,
+
+`Em lo âu trước xa tắp đường mình
+Trái tim đập những điều không thể nói
+Trái tim đập cồn cào cơn đói
+Ngọn lửa nào le lói giữa cô đơn
+
+- Xuân Quỳnh -`,
+
+`Em trở về đúng nghĩa trái-tim-em
+Là máu thịt, đời thường ai chẳng có
+Cũng ngừng đập lúc cuộc đời không còn nữa
+Nhưng biết yêu anh cả khi chết đi rồi
+
+- Xuân Quỳnh -`,
+
+`Cây dài bóng xế ngẩn ngơ...
+- Hồn em đã chín mấy mùa thương đau?
+Tay anh em hãy tựa đầu,
+Cho anh nghe nặng trái sầu rụng rơi...
+
+- Huy Cận -`,
+
+
+
+
+];
---
+++ script.js
@@ -0,0 +1,21 @@
+let clicked = false;
+
+function randomPoem() {
+
+    if (clicked) {
+        return;
+    }
+
+    clicked = true;
+
+    const randomIndex =
+        Math.floor(Math.random() * poems.length);
+
+    document.getElementById("poem").innerText =
+        poems[randomIndex];
+
+    document.getElementById("btn").disabled = true;
+
+    document.getElementById("btn").innerText =
+        "Đã nhận thơ hôm nay";
+}
