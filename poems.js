7bfe86a3a1a27cc50a50c0e61df84f747c1dc652
lauries <bichphan.295@gmail.com>
2026-05-31 21:56:25 +0700

    Add files via upload

--- poems.js
+++ poems.js
@@ -7,14 +7,14 @@ Người ta phụ, hoặc thờ ơ, chẳng biết."
 
 - Xuân Diệu -`,
 
-`Máu đã khô rồi thơ cũng khô
+`"Máu đã khô rồi thơ cũng khô
 Tình ta chết yểu tự bao giờ!
 Từ nay trong gió, - trong mây gió,
-Lời thảm thương rền khắp nẻo mơ.
+Lời thảm thương rền khắp nẻo mơ."
 
 - Hàn Mặc Tử -`,
 
-`Tôi rót hồn tôi xuống mắt nàng.
+`"Tôi rót hồn tôi xuống mắt nàng.
 Hồn tôi là cả một lời van.
 Tôi van nàng đấy! Van nàng đấy!
 Ai có yêu đương chả vội vàng?
@@ -22,7 +22,7 @@ Ai có yêu đương chả vội vàng?
 Tôi rót hồn tôi xuống đã nhiều,
 Hồn tôi còn có được bao nhiêu?
 Tôi đi sợ cả lời tôi nói,
-Sợ cả gần nàng, sợ cả yêu.
+Sợ cả gần nàng, sợ cả yêu."
 
 - Nguyễn Bính -`,
 
@@ -52,54 +52,54 @@ Một nửa hồn tôi bỗng dại khờ."
 
 - Hàn Mặc Tử -`,
 
-`Ta trút linh hồn giữa lúc đây,
+`"Ta trút linh hồn giữa lúc đây,
 Gió sầu vô hạn nuối trong cây...
 - Còn em sao chẳng hay gì cả?
-Xin để tang anh đến vạn ngày.
+Xin để tang anh đến vạn ngày."
 
 - Hàn Mặc Tử -`,
 
-`Tôi trả cho tôi những ngại ngần
+`"Tôi trả cho tôi những ngại ngần
 Trả người - đây nhé những phân vân
 Cõi riêng lặng lẽ gài then kín
-Ngoài ấy người vui với bụi trần.
+Ngoài ấy người vui với bụi trần."
 
 - Hàn Mặc Tử -`,
 
-`Tôi khát vô cùng
+`"Tôi khát vô cùng
 Tôi giết thời gian trong nắm tay
 Tôi vò tiếc mến như vo lụa
-Cất tiếng cười giòn xao động vùng mây
+Cất tiếng cười giòn xao động vùng mây"
 
 - Hàn Mặc Tử -`,
 
-`Sóng bắt đầu từ gió
+`"Sóng bắt đầu từ gió
 Gió bắt đầu từ đâu?
 Em cũng không biết nữa
-Khi nào ta yêu nhau
+Khi nào ta yêu nhau"
 
 - Xuân Quỳnh -`,
 
-`Ở ngoài kia đại dương
+`"Ở ngoài kia đại dương
 Trăm nghìn con sóng đó
 Con nào chẳng tới bờ
-Dù muôn vời cách trở
+Dù muôn vời cách trở"
 
 - Xuân Quỳnh -`,
 
-`Nếu tất cả đường đời đều trơn láng,
-Chắc gì ta đã nhận ra ta
+`"Nếu tất cả đường đời đều trơn láng
+Chắc gì ta đã nhận ra ta"
 
 - Lưu Quang Vũ -`,
 
-`Dù đục dù trong, con sông vẫn chảy
+`"Dù đục dù trong, con sông vẫn chảy
 Dù cao dù thấp, cây lá vẫn xanh
 Dù người phàm tục hay kẻ tu hành
-Vẫn phải sống từ những điều rất nhỏ
+Vẫn phải sống từ những điều rất nhỏ"
 
 - Lưu Quang Vũ -`,
 
-`Chỉ có thuyền mới hiểu
+`"Chỉ có thuyền mới hiểu
 Biển mênh mông nhường nào
 Chỉ có biển mới biết
 Thuyền đi đâu, về đâu
@@ -107,30 +107,45 @@ Thuyền đi đâu, về đâu
 Những ngày không gặp nhau
 Biển bạc đầu thương nhớ
 Những ngày không gặp nhau
-Lòng thuyền đau - rạn vỡ
+Lòng thuyền đau - rạn vỡ"
 
 - Xuân Quỳnh -`,
 
-`Em lo âu trước xa tắp đường mình
+`"Em lo âu trước xa tắp đường mình
 Trái tim đập những điều không thể nói
 Trái tim đập cồn cào cơn đói
-Ngọn lửa nào le lói giữa cô đơn
+Ngọn lửa nào le lói giữa cô đơn"
 
 - Xuân Quỳnh -`,
 
-`Em trở về đúng nghĩa trái-tim-em
+`"Em trở về đúng nghĩa trái-tim-em
 Là máu thịt, đời thường ai chẳng có
 Cũng ngừng đập lúc cuộc đời không còn nữa
-Nhưng biết yêu anh cả khi chết đi rồi
+Nhưng biết yêu anh cả khi chết đi rồi"
 
 - Xuân Quỳnh -`,
 
-`Cây dài bóng xế ngẩn ngơ...
+`"Cây dài bóng xế ngẩn ngơ...
 - Hồn em đã chín mấy mùa thương đau?
 Tay anh em hãy tựa đầu,
-Cho anh nghe nặng trái sầu rụng rơi...
+Cho anh nghe nặng trái sầu rụng rơi..."
 
 - Huy Cận -`,
+
+`"Tôi vẫn còn đây hay ở đâu?
+Ai đem tôi bỏ dưới trời sâu?
+Sao bông phượng nở trong màu huyết,
+Nhỏ xuống lòng tôi những giọt châu?"
+
+- Hàn Mặc Tử -`,
+
+`"Nhưng làm sao lòng ta luôn nơm nớp
+Và bao giờ cũng muốn tránh nhau ra,
+Tình cao riêng muôn đời không tái hợp
+Vì lòng ơi! Ngao ngán đến thời xa."
+
+- Hàn Mặc Tử -`
+
 
 
 
